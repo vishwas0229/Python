58932f42ca9873e61f94519d3a832eb7c28e812a
vishwas0229 <852006rahul@gmail.com>
2026-01-21 01:25:27 +0530

    Add tasks

---
+++ Module-1/task-1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+print("\nAddition = ", num1+num2)
+print("Subtraction = ", num1-num2)
+print("Multiplication = ", num1*num2)
+print("Division = ", num1/num2)
---
+++ Module-1/task-2.py
@@ -0,0 +1,4 @@
+first_name = str(input("Enter Your First Name: "))
+last_name = str(input("Enter Your Last Name: "))
+full_name = first_name + ' ' + last_name
+print("\n\nHello!", full_name, "Welcome to the Python Program.")
