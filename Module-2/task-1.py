f97f805ebe2ce843f789f08b9f028c6e360dada1
Vishwas <vishwas@Vishwas.168.1.35>
2026-01-31 23:05:22 +0530

    Module 2

---
+++ Module-2/task-1.py
@@ -0,0 +1,8 @@
+#input a number from user
+num = int(input("Enter a number: "))
+
+#chech Odd/Even
+if num%2==0:
+    print(f"{num} is a even number.")
+else:
+    print(f"{num} is a odd number.")
---
+++ Module-2/task-2.py
@@ -0,0 +1,5 @@
+n=1
+total = 0
+for i in range(n,50+1):
+    total+=i
+print(f"The sum of numbers from {n} to 50 is {total}.")
